1213e46738a9f0d1cd52fca83aa34beca8a82b44
Tomer <tomerrim@gmail.com>
2023-04-28 20:04:40 +0300

    logging requests with morgan and create launch model

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
@@ -8,6 +10,13 @@ app.use(cors({
     origin: "http://localhost:3000"
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"..","public")));
 app.use(planetsRouter);
+
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname,"..","public","index.html"))
+});
+
+app.use(morgan("combined"));
 
 module.exports = app;
