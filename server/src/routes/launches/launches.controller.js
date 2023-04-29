64733e3c6f8e3aa4a7aab90f7fca6070ffb91e11
Tomer <tomerrim@gmail.com>
2023-04-29 12:56:29 +0300

    connect the POST /launches with the client

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,7 @@ function httpgetAllLaunches(req,res){
 
 function httpAddNewLaunch(req,res){
     const launch = req.body;
-    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination ){
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target ){
         return res.status(400).json({
             error: "Missing Required launch property",
         });
@@ -18,7 +18,7 @@ function httpAddNewLaunch(req,res){
           error: "Invalid launch date",
         });
     }
-    
+
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
