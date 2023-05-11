01ede604f93dc031a4753dda3233253d293b9436
Tomer <tomerrim@gmail.com>
2023-05-11 17:03:00 +0300

    added saveLaunch function and changed the getAllLaunches to get the data from db

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
-//const launches = require("./launches.mongo");
+const launchesDataBase = require("./launches.mongo");
+const planets = require("./planets.mongo");
 
 const launches = new Map();
 
@@ -15,14 +16,32 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
+//launches.set(launch.flightNumber, launch);
 
 function existsLaunchWithId(launchId){
     return launches.has(launchId);
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches() {
+  return await launchesDataBase
+  .find({},{"_id": 0, "__v": 0});
+}
+
+async function saveLaunch(launch){
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if(!planet){
+    throw new Error("No matching planet found")
+  }
+
+  await launchesDataBase.updateOne({
+    flightNumber: launch.flightNumber,
+  },launch,{
+    upsert: true,
+  });
 }
 
 function addNewLaunch(launch) {
