17b2d1208c121134bc414eb698439fc6c94fe42a
Tomer <tomerrim@gmail.com>
2023-05-11 10:47:58 +0300

    added mongoose and created mongoose schemas to planets and launches and connect mongoose on server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,28 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const port = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://tomerrim:vrNsREgBHwRW1W0p@nasacluser.porxv3z.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log(`MongoDB connection ready`);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`Error: ${err}`);
+});
+
 async function startServer(){
-    await loadPlanetsData();
-    server.listen(port, () => {
-      console.log(`Listening on port: ${port}`);
-    });
+  await mongoose.connect(MONGO_URL);
+  await loadPlanetsData();
+  server.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+  });
 }
 
 startServer();
